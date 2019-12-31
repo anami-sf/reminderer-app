8dffe5df27b1d11541823c8b3698cb9b5cbf960e
anami823 <anami823@gmail.com>
2019-12-30 18:41:34 -0800

    Config sign-in method

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,26 +2,45 @@ import React, { Component } from 'react'
 
 class Dashboard extends Component {
 
-    onClick = () => {
-        const URL = 'https://remindererapiv2.herokuapp.com/dashboard/index'
-        //const URL = 'http://localhost:3000/dashboard/index'
+  componentDidMount() {
+    this.signIn()
+  }
 
-        fetch( URL, { method: 'get', credentials: 'include' } )
-          .then((response) => {
-            console.log(response);
-            return response.json();
-          })
-          .then((myJson) => {
-            console.log(myJson);
-          });
+  signIn = () => {
+    const URL = 'https://remindererapiv2.herokuapp.com/auth/google_oauth2'
+    //const URL = 'http://localhost:3001/dashboard/index'
+
+    fetch( URL, { method: 'get', credentials: 'include'} )
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((myJson) => {
+        console.log(myJson);
+      });
+
+}
+
+
+    // onClick = () => {
+    //     const URL = 'https://remindererapiv2.herokuapp.com/dashboard/index'
+    //     //const URL = 'http://localhost:3000/dashboard/index'
+
+    //     fetch( URL, { method: 'get' } )
+    //       .then((response) => {
+    //         console.log(response);
+    //         return response.json();
+    //       })
+    //       .then((myJson) => {
+    //         console.log(myJson);
+    //       });
   
-    }
+    // }
 
     render() {
       return (
         <>
         <h1>Dashboard</h1> 
-        {this.onClick()}
         </>       
       )
     }
